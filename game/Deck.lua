ae1320de4097e1e10b76d1135ef6edf55f49375a
Tobias Gärdebro <tubbeg@gmail.com>
2025-08-13 23:00:39 +0200

    Working on simplifying input for the game.

--- game/Deck.lua
+++ game/Deck.lua
@@ -1,189 +1,83 @@
-
-
---[[
-
-NOTE: The z-index has been removed. The drawing is based on
-which element comes first in the table, i.e. the table index
-
-So it's essentially the same thing. I realized that the Deck
-class should be responsible for keeping track of the z-index,
-and not the individual sprites
-]]
-
-local Sprite = require ("Sprite")
+local Sprite = require ("Sprite2")
 local Deck = {}
 
-function Deck:new (startX, startY, endX)
+function Deck:new (startX, startY, endX, playLocation)
     local deck = {}
     deck.cards = {}
-    deck.draggedCard = nil
     deck.position = {startX = startX, startY = startY, endX = endX}
     deck.range = deck.position.endX - deck.position.startX
     deck.px = nil
     deck.nr = nil
-    deck.isPlayingCard = false -- multiple cards cannot be played at the same time
+    deck.center = playLocation
+    deck.selected = 1
     self.__index = self
     return setmetatable(deck, self)
 end
 
 function Deck:draw()
-    if not self.draggedCard then
-        for i,s in ipairs(self.cards) do
-            s:draw()
-        end
-    else
-        for i,s in ipairs(self.cards) do
-            if s ~= self.draggedCard then
-                s:draw()
-            end
-        end
-        self.draggedCard:draw()
+    for i,s in ipairs(self.cards) do
+        s:draw(self.cards[self.selected])
     end
 end
 
-function Deck:SetNrOfCards(nr)
+function Deck:setNrOfCards(nr)
     if self.range then
         self.px = (self.range/nr) - 5
         self.nr = nr
     end
 end
 
-function Deck:GetNextPos(i)
-    local position = {}
-    position.y = self.position.startY
-    position.x = self.position.startX + (self.px * (i - 1))
-    return position
+function Deck:getNextPos(i)
+    return
+        {
+            y=self.position.startY,
+            x=self.position.startX + (self.px * (i - 1))
+        }
 end
 
 function Deck:load(keys)
+    self:setNrOfCards(#keys)
     for i,k in ipairs(keys) do
-        local s = Sprite:new(self:GetNextPos(i), k.id, keys.prop)
-        table.insert(self.cards, s)
-    end
-end
-
--- I could also set the element to nil
--- but it causes a bug where other cards
--- also disappear. It might have to do something
--- with the sorting function
-function RemoveElement(t,index)
-    local newTable = {}
-    for i,v in ipairs(t) do
-        if i ~= index then
-            table.insert(newTable,v)
+        local s = Sprite:new(k.id, self:getNextPos(i)):load()
+        if s then
+            table.insert(self.cards, s)
         end
     end
-    return newTable
 end
 
-
-function Deck:RemoveCompletedCards(index,s,dt)
-    if s:AnimationComplete() then
-        self.cards = RemoveElement(self.cards, index)
-        self:SetNrOfCards(#self.cards)
-        for j,card in ipairs(self.cards) do
-            card.origin = self:GetNextPos(j)
-            card:TweenToOrigin(dt)
-        end
-        self.isPlayingCard = false
-    end
-end
 
 function Deck:update(dt)
     for i,s in ipairs(self.cards) do
-        s:Update(dt)
-        self:RemoveCompletedCards(i,s,dt)
-    end
-    self:StopDrag()
-    self:HandleCollision()
-end
-
-function Deck:ResetDrag()
-    for i,s in ipairs(self.cards) do
-        s:resetDrag()
+        s:update(dt)
     end
 end
 
-function Deck:SetCardOnLeftClick(x,y,button)
-    if button == 1 then
-        for i,s in ipairs(self.cards) do
-            if s:posOverlap({x=x,y=y}) then
-                self.draggedCard = s
+
+function Deck:click()
+    local s = self.cards[self.selected]
+    if s and self.center then
+        s:play(self.center)
+        if #self.cards >= 1 then
+            self.selected = 1
+        end
+    end
+end
+
+function Deck:changeSelection(direction)
+    local i = #self.cards
+    if i > 1 then
+        if direction == "left" then
+            self.selected = self.selected - 1
+            if self.selected < 1 then
+                self.selected = i
+            end
+        else
+            self.selected = self.selected + 1
+            if self.selected > i then
+                self.selected =  1
             end
         end
     end
 end
-
-
-function PlayAnimation(card)
-    -- TBD
-end
-
-function Deck:PlayCard(s)
-    if not self.isPlayingCard then
-        self.isPlayingCard = true
-        s:Play({x=300, y=100})
-    end
-end
-
-
-function Deck:SelectCardOnRightClick(x,y,button)
-    if button == 2 then
-        for i,s in ipairs(self.cards) do
-            if s:posOverlap({x=x,y=y}) then
-                self:PlayCard(s)
-            end
-        
-        end 
-    end
-end
-
-function Deck:click(x,y,button)
-    self:ResetDrag()
-    if not self.isPlayingCard then
-        self:SetCardOnLeftClick(x,y,button)
-        self:SelectCardOnRightClick(x,y,button)
-        if self.draggedCard and not self.isPlayingCard then
-            self.draggedCard:setDrag({x=x,y=y})
-        end
-    end
-end
-
-function Deck:StopDrag()
-    if not love.mouse.isDown(1) then
-        for i,s in ipairs(self.cards) do
-            self.draggedCard = nil
-            s:resetDrag()
-        end
-    end
-end
-
-function SortByPos (a,b)
-    return a.origin.x > b.origin.x
-end
-
-function Deck:UpdateZindex()
-    table.sort(self.cards, SortByPos)
-end
-
-
-function Deck:SwapCardsOnCollision(sprite)
-    if sprite ~= self.draggedCard then
-        if sprite:HasCollided(self.draggedCard) then
-            sprite:SwapOrigin(self.draggedCard)
-            sprite:DisableCollisionUntilOrigin()
-            self:UpdateZindex()
-        end
-    end
-end
-
-function Deck:HandleCollision()
-    if self.draggedCard and not self.isPlayingCard then
-        for i,s in ipairs(self.cards) do
-            self:SwapCardsOnCollision(s)
-        end
-    end
-end
-
 
 return Deck
--- game/main.lua
+++ game/main.lua
@@ -1,12 +1,14 @@
 local Sprite = require ("Sprite")
 local Deck = require ("Deck")
 
+local wParam = {x=800,y=600}
 local startAtX = 50
 local endAtX = 700
 local startAtY = 300
-local deck = Deck:new(startAtX,startAtY,endAtX)
+local playTo = {x=wParam.x/2.4, y=wParam.y/6}
+local deck = Deck:new(startAtX,startAtY,endAtX,playTo)
 
-local dummyId = "sprite.png"
+local dummyId = "assets/aseprite/bomb.png"
 local dummyKey = {id = dummyId, prop = {}}
 local keys =
     {
@@ -20,13 +22,31 @@ local keys =
         [8] = dummyKey
     }
 
-function love.mousepressed(x, y, button)
-    deck:click(x,y,button)
+function love.keyreleased( key )
+   if key == "x" then
+        deck:click()
+   end
+   if key == "z" then
+        print("TBD")
+   end
+   if key == "left" then
+        deck:changeSelection("left")
+   end
+   if key == "right" then
+        deck:changeSelection("right")
+   end
+   if key == "z" then
+        print("TBD")
+   end
 end
 
 function love.load()
-    deck:SetNrOfCards(#keys)
-    deck:load(keys)
+    if love.window.setMode( wParam.x, wParam.y) then
+        love.graphics.setDefaultFilter("nearest", "nearest")
+        deck:load(keys)
+    else
+        print("Something went wrong :/")
+    end
 end
 
 function love.update(dt)
@@ -34,5 +54,6 @@ function love.update(dt)
 end
 
 function love.draw()
+
     deck:draw()
 end
